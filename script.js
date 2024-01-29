bba790f452e8b62773062b91fe9ca3283382981f
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-01-29 18:02:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// getElementById
+//  const heading= document.getElementById("head")
+
+
+q           // getElementsByTageName
+// const tag =document.getElementsByTagName('h1')
+
+
+                        // getElementByClassName
+// const head =document.getElementsByClassName('headiing')
+//  console.log(head)
+
+                // query selector - eske andar hm css type ke selector likhte hai
+// const heading =document.querySelector('#heading');  
+// const head =document.querySelector('.heading');  
+// console.log(heading);
+// console.log(head)
+
+                            // query selectorAll - jaise tag name or class name to get kr rha tha waise hi sabhi elements ke class ko return krega 
+
+// const head =document.querySelectorAll('.heading');  
+// console.log(heading);
+// console.log(head)
+
+// traversal in dom jaise h1ke phle wala element get krna ho jaise child or parent element
+// parentnode
+// const heading=document.querySelector('.heading');
+// const parent =heading.parentNode;
+// console.log(parent)
+
+
+// children node
+const parent =document.querySelector('.parent');
+
+console.log(parent.childNodes);
+
